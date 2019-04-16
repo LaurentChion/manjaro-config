c56a41df80e91bd7e07513275dab7b20d0abdf1d
Laurent Chion <laurent.chion@gmail.com>
2019-04-16 21:34:34 +0200

    Change right and fix script

--- install_font.sh
+++ install_font.sh
@@ -1,5 +1,6 @@
+mkdir large_files
 #Download font
-curl -L -o SourceCodePro.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/SourceCodePro.zip
+curl -L -o large_files/SourceCodePro.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/SourceCodePro.zip
 
 # Unzip it to ~/.fonts/
 xarchiver -x ~/.fonts large_files/SourceCodePro.zip
