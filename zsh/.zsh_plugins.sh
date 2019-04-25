ac51735b866f0c179697020c0ded231d360be763
Laurent Chion <laurent.chion@gmail.com>
2019-04-25 14:22:37 +0200

    Add docker-compose shortcut

--- zsh/.zsh_plugins.sh
+++ zsh/.zsh_plugins.sh
@@ -21,6 +21,7 @@ source /home/laurent/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-
 fpath+=( /home/laurent/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-bhilburn-SLASH-powerlevel9k )
 source /home/laurent/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/git/git.plugin.zsh
 source /home/laurent/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/z/z.plugin.zsh
+source /home/laurent/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/docker-compose/docker-compose.plugin.zsh
 source /home/laurent/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-djui-SLASH-alias-tips/alias-tips.plugin.zsh
 fpath+=( /home/laurent/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-djui-SLASH-alias-tips )
 source /home/laurent/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
